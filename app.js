aa109e2a8a86f3eff0b1c16954a27282cd78f832
jajaka111 <74236737+jajaka111@users.noreply.github.com>
2020-11-27 19:01:24 +0700

    express

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 import express from 'express'
 const app = express()
-const port = 8000
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "aprogramm2r.com"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});    
 
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
 app.use(cors())
